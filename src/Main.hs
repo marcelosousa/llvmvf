0990eab10fee0f38494481de039c6e61918910c3
Marcelo Sousa <msousa@clpc121.cs.ox.ac.uk>
2013-02-19 22:46:28 +0000

    improved user interface

--- src/Main.hs
+++ src/Main.hs
@@ -29,35 +29,65 @@ import Concurrent.Model
 -- import Concurrent.Model.SystemC
 import Concurrent.Model.Visualizer
 -- import Concurrent.Model.ESEncoder  (esencode)    
---import Concurrent.Model.Encoder    (encode, encodeSysC)    
+--import Concurrent.Model.Encoder    (encode) 
+-- , encodeSysC)    
 
 import Util.Demangler
 import Debug.Trace
 import Analysis.Memory.TyAnnInf
 
--- This module needs re-factoring for elegance.
+_program, _summary :: String
+_summary = unlines ["LLVM Verification Framework - v0.2","llvmvf supports verification of pthread code at the LLVM IR level.","Copyright 2012 @ Marcelo Sousa"]
+_program = "llvmvf"
+_help    = "The input files of llvmvf are byte code files generated from a LLVM front-end (eg. clang)"
+_helpBMC = "Example: llvmvf bmc -d=pthread -b=10 x.bc"
+_helpExtract = unlines ["The extract command is useful for front-end debug.","llvmvf extract generates a .llvf file that is a pretty printed version of the internal representation.","Example: llvmvf extract x.bc"]
 
--- Options 
-data Options = Parse
-             | Visualize
-             | Extract
-             | SystemC
-             | Htm
-             | Type
-  deriving (Show, Data, Typeable)
+data Option = Extract   {input :: FilePath}
+            | Visualize {input :: FilePath}
+            | BMC       {input :: FilePath, domain :: Domain, bound :: Int}
+            | Convert   {input :: FilePath}
+            | TypeCheck {input :: FilePath}
+  deriving (Show, Data, Typeable, Eq)
 
-instance Default Options where
-  def = Parse
+data Domain = PThread | SystemC
+  deriving (Show, Data, Typeable, Eq)
 
-runOption :: FilePath -> Options -> Int -> IO ()
-runOption bc Extract   _ = do mdl <- extract bc
-                              let bf = dropExtension bc
-                              writeFile (addExtension bf "llvf") (show $ pretty mdl)
-runOption bc Htm _ = do mdl <- extract bc
-                        let bf = dropExtension bc
-                        writeFile (addExtension bf "htm") (show $ pretty $ llvmir2Htm mdl)
-runOption bc Type _ = do mdl <- extract bc
-                         print $ modTyInf mdl
+instance Default Domain where
+  def = PThread
+
+extractMode :: Option
+extractMode = Extract  { input = def &= args } &= help _helpExtract
+
+bmcMode :: Option
+bmcMode = BMC { input = def &= args
+              , domain = def &= help "domain of verification: PThread | SystemC (Super Beta)" 
+              , bound  = def &= help "bound (k): Int"
+              } &= help _helpBMC
+
+
+progModes :: Mode (CmdArgs Option)
+progModes = cmdArgsMode $ modes [extractMode, bmcMode]
+         &= help _help
+         &= program _program
+         &= summary _summary
+
+-- | 'main' function 
+main :: IO ()
+main = do options <- cmdArgsRun progModes
+          runOption options
+          
+runOption :: Option -> IO ()
+runOption (Extract bc) = do mdl <- extract bc
+                            let bf = dropExtension bc
+                            writeFile (addExtension bf "llvf") (show $ pretty mdl)
+{-runOption bc Htm     = do mdl <- extract bc
+                          let bf = dropExtension bc
+                          writeFile (addExtension bf "htm") (show $ pretty $ llvmir2Htm mdl)
+runOption bc Type = do mdl <- extract bc
+                       print $ modTyInf mdl
+-}
+runOption (BMC bc d k) = print $ "Working" ++ show bc ++ show d ++ show k
 --runOption bc Parse k = do mdl <- extract bc
 --                          let bf  = dropExtension bc
 --                              mod = (model mdl) :: Model PThread
@@ -82,30 +112,3 @@ runOption bc Type _ = do mdl <- extract bc
 --                              writeFile (addExtension bf "dot")   (show $ pretty mod)
 --                              writeFile (addExtension bf "rawm")  (show $ mdl)
 --                              writeFile (addExtension bf "smt2")  (show $ prettyprint $ encodeSysC mod k)
-
-data ProgramOptions = LLVMVF {
-    input  :: FilePath
-  , typeoutput :: Options
-  , bound :: Int
-}
-  deriving (Show, Data, Typeable)
-
-standard = cmdArgsMode $ LLVMVF 
-           { 
-             input         = (def &= args )
-           , typeoutput    = (def &= help "Parse" &= typ "Parse")
-           , bound         = (def &= help "bound" &= opt (1 :: Int))
-           } &= summary usage
-
-main :: IO ()
-main = do args <- cmdArgsRun standard
-          runllvmvf args
-          
-runllvmvf :: ProgramOptions -> IO ()
-runllvmvf options = do let filename = input options
-                           option   = typeoutput options
-                           k        = bound options
-                       runOption filename option k
-
-usage :: String
-usage = unlines ["LLVM Verification Framework - v1.0b"]
