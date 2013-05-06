9e5dd02228ca56d98e7b53dea3f4a4a9a4fe7306
Marcelo Sousa <dipython@gmail.com>
2013-05-06 20:53:05 +0100

    re-factoring

--- src/Concurrent/Model.hs
+++ src/Concurrent/Model.hs
@@ -7,7 +7,7 @@
 module Concurrent.Model where
 
 import qualified Data.IntMap as IM
-import qualified Data.Map as Map
+import qualified Data.Map    as M
 
 import Language.LLVMIR
 import Language.LLVMIR.Util
@@ -26,51 +26,29 @@ import Debug.Trace
 -- being executed. Scheduler specification is required.
 class SCModel t where
   model       :: Module  -> Model t
-  controlflow :: Model t -> ControlFlow
-  controlflow m@Model{..} = cflowfs $ getFs mainf procs 
-  dataflow    :: Model t -> DataFlow
-  dataflow    m@Model{..} = dflowfs $ getFs mainf procs
+  prune       :: String  -> Model t -> (Model t, ControlFlow, DataFlow)
+--  controlflow :: Model t -> ControlFlow
+--  controlflow m@Model{..} = cflowfs $ getFs mainf procs 
+--  dataflow    :: Model t -> DataFlow
+--  dataflow    m@Model{..} = dflowfs $ getFs mainf procs
  
-getFs :: Process -> Processes -> Functions
-getFs p ps = let pss = IM.elems ps
-                 fs  = foldr (\p' r -> Map.insert (ident p') (unProc p') r) Map.empty pss 
-                 fs' = Map.insert (ident p) (unProc p) fs
-             in fs'
+-- Monad Kind
+-- if possible a type definition would be better
+data Model t = Model Module
 
-toFunctions :: Processes -> Functions
-toFunctions ps = let pss = IM.elems ps
-                 in  foldr (\p' r -> Map.insert (ident p') (unProc p') r) Map.empty pss 
-
--- Program P has a set of threads and a set of shared variables
--- V  - Global variables (gvars)
--- add scheduler type
-data Model t = Model { nmdtys :: NamedTypes -- This information should not be here
-                     , gvars  :: Globals
-                     , mainf  :: Process
-                     , procs  :: Processes
-                     , decls  :: Declarations -- This information should be in the dataflow
-                     } 
-
-data Process = Process { ident :: String, unProc :: Function }
-
-emptyProcess :: Process
-emptyProcess = Process "undefined" emptyFunction
-
-type Declarations = Map.Map String (Type, Parameters) 
-type Processes = IM.IntMap Process
-
-type ThreadLabel = Int
+---------------------------------------------------
+-- REMOVE THIS PART FROM THIS FILE
 type Bound = Int
-type TypeEnv   = Map.Map Type (SSortExpr, SSort)
+type TypeEnv   = M.Map Type (SSortExpr, SSort)
 
 emptyPreEncoder :: PreEncoder
-emptyPreEncoder = PreEncoder Map.empty Map.empty [] Map.empty Map.empty []
+emptyPreEncoder = PreEncoder M.empty M.empty [] M.empty M.empty []
 -- 
-data PreEncoder = PreEncoder { argToPar :: Map.Map (PC,Int,Value) Id   -- Map an argument to a parameter -- Do not support calling the same function twice. New fresh variables
-                             , fStore   :: Map.Map Id (Type, [PC])     -- Map a global variable to a list of program counter that store a new value
+data PreEncoder = PreEncoder { argToPar :: M.Map (PC,Int,Value) Id   -- Map an argument to a parameter -- Do not support calling the same function twice. New fresh variables
+                             , fStore   :: M.Map Id (Type, [PC])     -- Map a global variable to a list of program counter that store a new value
                              , mutexes  :: [Id]
                              , sortEnv  :: TypeEnv                     -- Map all the types to a sort expression and a sort name
-                             , locals   :: Map.Map Id Type             -- Map all identifiers to a type
+                             , locals   :: M.Map Id Type             -- Map all identifiers to a type
                              , fails    :: [PC]                        -- List of program counters that call assert_fail
                              }
 
@@ -83,17 +61,17 @@ instance Show PreEncoder where
                              ++ show l ++ "\n-----------------\n"
                              ++ show f
 
-type Valuation = Map.Map Id (Either Id Value)
+type Valuation = M.Map Id (Either Id Value)
 
 -- GlobalState of a Concurrent System
--- type GlobalState = (Map.Map String (PC, Map.Map Id Value), Map.Map Id Value, PC)
+-- type GlobalState = (M.Map String (PC, M.Map Id Value), M.Map Id Value, PC)
 nullGlobalState :: GlobalState
-nullGlobalState = GlobalState Map.empty (-1) Map.empty Map.empty
+nullGlobalState = GlobalState M.empty (-1) M.empty M.empty
 
 data GlobalState = GlobalState { defsorts  :: TypeEnv
                                , currentpc :: PC
                                , gvals     :: Valuation
-                               , ti        :: Map.Map Identifier ThreadState
+                               , ti        :: M.Map Identifier ThreadState
                                }
   deriving Show
 
@@ -116,10 +94,4 @@ fromISExpr ISEmpty        = error "ISEmpty"
 fromISExpr (ISFunction f) = error "ISFunction"
 
 instance Show (Model t) where
-  show (Model nmdtys gvars mainf procs decls) = show mainf ++ "\n" ++ show procs ++ "\n" ++ show decls
-
-instance Pretty Function where
-    pretty f = pp_Syn_Function $ wrap_Function (sem_Function f) $ Inh_Function {}
-
-instance Show Process where
-  show (Process i f) = show $ pretty f
+  show (Model mod) = show mod
