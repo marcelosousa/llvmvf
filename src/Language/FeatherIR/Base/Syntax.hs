4829b93625e01b9ea06086e3fd42eac83020511a
marcelosousa <dipython@gmail.com>
2013-07-23 17:05:21 -0700

    more formalization of featherweight llvm ir

--- src/Language/FeatherIR/Base/Syntax.hs
+++ src/Language/FeatherIR/Base/Syntax.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeOperators, UnicodeSyntax #-}
 -------------------------------------------------------------------------------
 -- Module    :  Language.FeatherIR.Base.Syntax
 -- Copyright :  (c) 2013 Marcelo Sousa
@@ -11,15 +11,19 @@ import Language.FeatherIR.Base.Type
 import Data.Map
 
 type Id = String
-type (:⇀) = Map
 
-data Σ = Σ [(Id,Τ)] Τ
 
-data P = P (Id:⇀Τ) (Id:⇀G) (Id:⇀F)
+data P = 
+	P { namedTypes      ∷ Map Id Τ 
+	  , globalVariables ∷ Map Id G 
+	  , functions       ∷ Map Id F
+	}
 
 data G = G Id Τ C
 
 data F = F Id Σ [BB]
+
+data Σ = Σ [(Id,Τ)] Τ
 
 -- Basic Block
 data BB = BB Id [Φ] [I] T
@@ -28,7 +32,27 @@ data BB = BB Id [Φ] [I] T
 data Φ = Φ Id Τ [(V,Id)]
 
 -- instruction
-data I = I
+data I =
+	  BinOp Id Τ BinI V V
+	| BitOp Id Τ BitI V V
+    | DerOp DerI
+    | Cast  Id Τ V
+    | Call  Id Τ Id [V]
+    | Cmp   Id Τ V V
+    | MemOp MemI
+
+data BinI =
+	Add | Sub | Mul | Div | Rem
+
+data BitI = 
+	Shl | LShr | AShr | And | Or | Xor
+
+data DerI = 
+	  Extract Id Τ V [V]
+	| Insert  Id Τ V V [V]
+
+data MemI = 
+	  Alloca Id Τ V
 
 -- terminator
 data T = T
@@ -37,5 +61,7 @@ data T = T
 data C = Constant
 
 -- value
-data V = VI Id | VC C
+data V = 
+	  VI Id Τ
+	| VC C
 
